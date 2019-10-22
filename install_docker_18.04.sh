8cca4c715396eb5b7b8abb6e83863df7e2a0f43d
shyuniz <shyuniz@naver.com>
2019-10-22 17:02:18 +0900

    modify docker setting

--- install_docker_18.04.sh
+++ install_docker_18.04.sh
@@ -47,7 +47,7 @@ sudo apt install docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo chmod 755 *.sh
+sudo chmod +x *.sh
 ./startup.sh
 ./stop.sh
 
