4805d33d9aa5223567c197b31394c1ab97389cd2
shyuniz <shyuniz@naver.com>
2019-10-22 16:26:09 +0900

    add sudo command

--- install_docker_18.04.sh
+++ install_docker_18.04.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-if [ -z "$1" ] || [ -z "$2" ]
+if [ -z "$1" ]
 then
-  echo "Please Insert arguments ( ./install_docker_18.04.sh [APPLICATION_NAME] [USER_GROUP_NAME] )"
-  echo "ex:) ./install_docker_18.sh basic_webserver loenzo"
+  echo "Please Insert arguments ( ./install_docker_18.04.sh [APPLICATION_NAME] )"
+  echo "ex:) ./install_docker_18.sh basic_webserver"
   exit 1
 fi
 
 APPLICATION_NAME=$1
-GROUP_NAME=$2
 
 #CREATE DIRECTORY
 echo "#######################"
@@ -18,35 +17,33 @@ echo "# 1. ./000_application/$APPLICATION_NAME"
 echo "# 2. ./001_database/data"
 echo "# 2. ./001_database/backup"
 
-mkdir -p ./000_application/$APPLICATION_NAME
-mkdir -p ./001_database/data
-mkdir -p ./001_database/backup
+sudo mkdir -p ./000_application/$APPLICATION_NAME
+sudo mkdir -p ./001_database/data
+sudo mkdir -p ./001_database/backup
 
-chmod -R 755 ./000_application  # 접근권한
-chown -R $GROUP_NAME ./000_application  #사용자설정
-chgrp -R $GROUP_NAME ./000_application  #사용자그룹설정
-chmod 644 ./001_database/conf.d/my.cnf #CONFIG 권한
+sudo chmod -R 755 ./000_application  # 접근권한
+sudo chmod 644 ./001_database/conf.d/my.cnf #CONFIG 권한
 
 #echo "## setting mariadb default time zone (1: delete default_time_zone)"
-#sed -i '/default_time_zone/d' ./001_database/conf.d/my.cnf
+#sudo sed -i '/default_time_zone/d' ./001_database/conf.d/my.cnf
 #
 #echo "## setting docker"
-#cd ./004_docker
+#sudo cd ./004_docker
 #
-#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-#apt update -y
-#apt autoremove -y
-#apt update -y
-#apt upgrade -y
+#sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt update -y
+#sudo apt autoremove -y
+#sudo apt update -y
+#sudo apt upgrade -y
 #
 #sudo apt update
 #sudo apt install apt-transport-https ca-certificates curl software-properties-common
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 #sudo apt update
-#apt-cache policy docker-ce
+#sudo apt-cache policy docker-ce
 #
-#apt install docker-ce
+#sudo apt install docker-ce
 #
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
