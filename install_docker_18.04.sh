8df1f29d8f6f8aee1ecbab14790a318feae316b5
shyuniz <shyuniz@naver.com>
2019-10-22 18:10:52 +0900

    db test

--- install_docker_18.04.sh
+++ install_docker_18.04.sh
@@ -37,14 +37,14 @@ sudo apt upgrade -y
 
 sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
 sudo apt-cache policy docker-ce
 
 sudo apt install docker-ce
 
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo chmod +x *.sh
