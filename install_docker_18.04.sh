8ace06a377ea0696e04cb9873053bc229d71499c
shyuniz <shyuniz@naver.com>
2019-10-22 16:32:40 +0900

    test modify command

--- install_docker_18.04.sh
+++ install_docker_18.04.sh
@@ -24,42 +24,42 @@ sudo mkdir -p ./001_database/backup
 sudo chmod -R 755 ./000_application  # 접근권한
 sudo chmod 644 ./001_database/conf.d/my.cnf #CONFIG 권한
 
-#echo "## setting mariadb default time zone (1: delete default_time_zone)"
-#sudo sed -i '/default_time_zone/d' ./001_database/conf.d/my.cnf
-#
-#echo "## setting docker"
-#sudo cd ./004_docker
-#
-#sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-#sudo apt update -y
-#sudo apt autoremove -y
-#sudo apt update -y
-#sudo apt upgrade -y
-#
-#sudo apt update
-#sudo apt install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-#sudo apt update
-#sudo apt-cache policy docker-ce
-#
-#sudo apt install docker-ce
-#
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#./startup.sh
-#./stop.sh
-#
-#echo "## setting mariadb default time zone (2: add default_time_zone)"
-#echo 'default_time_zone	= Asia/Seoul' >> ./001_database/conf.d/my.cnf
-#./startup.sh
-#
-#echo "## Connect Mariadb"
-#echo '###############################################################################################'
-#echo '#STEP 1. mysql -uroot -p < /etc/mysql/backup/[file name].sql'
-#echo '#STEP 2. exit'
-#echo '###############################################################################################'
-#./conn_mariadb.sh
-#
-#echo "## INSTALL HAS DONE"
-#echo "#######################"
+echo "## setting mariadb default time zone (1: delete default_time_zone)"
+sudo sed -i '/default_time_zone/d' ./001_database/conf.d/my.cnf
+
+echo "## setting docker"
+sudo cd ./004_docker
+
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt update -y
+sudo apt autoremove -y
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+./startup.sh
+./stop.sh
+
+echo "## setting mariadb default time zone (2: add default_time_zone)"
+echo 'default_time_zone	= Asia/Seoul' >> ./001_database/conf.d/my.cnf
+./startup.sh
+
+echo "## Connect Mariadb"
+echo '###############################################################################################'
+echo '#STEP 1. mysql -uroot -p < /etc/mysql/backup/[file name].sql'
+echo '#STEP 2. exit'
+echo '###############################################################################################'
+./conn_mariadb.sh
+
+echo "## INSTALL HAS DONE"
+echo "#######################"
